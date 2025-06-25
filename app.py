4eacf0153cf8346c89502c6698608466853e5bec
wilson7777777 <wilsononline2@yahoo.com>
2025-06-25 11:38:29 +0100

    Add Hello world

---
+++ app.py
@@ -0,0 +1,8 @@
+
+
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    print(hello_world())
+    # This will only run if the script is executed directly, not when imported
